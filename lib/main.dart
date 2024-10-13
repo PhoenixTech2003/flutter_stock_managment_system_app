34f3e3157434dae76ba483a97678755972aae9b3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-13 15:38:02 +0200

    Set up navigation and pages for each page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stock_managment_system_app/pages/inventory.dart';
+import 'package:flutter_stock_managment_system_app/pages/home.dart';
+import 'package:flutter_stock_managment_system_app/pages/reports.dart';
+import 'package:flutter_stock_managment_system_app/pages/users.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,18 +59,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int currentIndexPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -86,42 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const [
+        HomePage(),
+        InventoryPage(),
+        ReportsPage(),
+        UsersPage()
+      ][currentIndexPage],
+
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentIndexPage = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.dashboard), label: "Home"),
+          NavigationDestination(
+              icon: Icon(Icons.inventory), label: "Inventory"),
+          NavigationDestination(icon: Icon(Icons.book), label: "Reports"),
+          NavigationDestination(icon: Icon(Icons.people), label: "Users")
+        ],
+        selectedIndex: currentIndexPage,
       ),
-      
-      bottomNavigationBar: NavigationBar(destinations: const [
-        NavigationDestination(icon: Icon(Icons.dashboard), label: "Home"),
-        NavigationDestination(icon: Icon(Icons.inventory), label: "Inventory"),
-        NavigationDestination(icon: Icon(Icons.book), label: "Reports"),
-        NavigationDestination(icon: Icon(Icons.people), label: "Users")
-      ]), 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
