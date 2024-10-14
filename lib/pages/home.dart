8f2f03e2936d27331bbf757b506fa923765d483f
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-14 16:36:48 +0200

    Format the sales chart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,54 +6,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (const Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            Expanded(
-                child: CustomCard(
-                  
-                    Icon(
-                      Icons.calendar_month,
-                      color: Colors.amber,
-                      size: 40,
-                    ),
-                    "Yearly Sales",
-                    "No Data",
-                    Colors.black))
-          ],
-        ),
-        
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: CustomCard(
-                  Icon(
-                    Icons.calendar_month_outlined,
-                    color: Colors.blue,
-                    size: 40,
-                  ),
-                  "Monthly Sales",
-                  "No Data",
-                  Color(0xFFFCD34D)),
-            ),
-            Expanded(
+    return const Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: (const Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            
+            children: [
+              Expanded(
+                  child: CustomCard(
+                    
+                      Icon(
+                        Icons.calendar_month,
+                        color: Colors.amber,
+                        size: 40,
+                      ),
+                      "Yearly Sales",
+                      "No Data",
+                      Colors.black))
+            ],
+          ),
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
                 child: CustomCard(
                     Icon(
-                      Icons.trending_up,
-                      color: Colors.green,
+                      Icons.calendar_month_outlined,
+                      color: Colors.blue,
                       size: 40,
                     ),
+                    "Monthly Sales",
                     "No Data",
-                    "No Data",
-                    Color(0xFF172554)))
-          ],
-        ),
-        Expanded(child: SalesBarChart())
-        
-      ],
-    ));
+                    Color(0xFFFCD34D)),
+              ),
+              Expanded(
+                  child: CustomCard(
+                      Icon(
+                        Icons.trending_up,
+                        color: Colors.green,
+                        size: 40,
+                      ),
+                      "No Data",
+                      "No Data",
+                      Color(0xFF172554)))
+            ],
+          ),
+          SizedBox(height: 30,),
+          Expanded(child: SalesBarChart())
+          
+        ],
+      )),
+    );
   }
 }
--- lib/widgets/sales_bar_chart.dart
+++ lib/widgets/sales_bar_chart.dart
@@ -1,15 +1,55 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class SalesBarChart extends StatelessWidget{
+class SalesBarChart extends StatelessWidget {
   const SalesBarChart({super.key});
   @override
   Widget build(BuildContext context) {
-    return(BarChart(
-  BarChartData(
-    // read about it in the BarChartData section
-  ),
-   // Optional
-));
+    return (BarChart(
+      BarChartData(
+        gridData: const FlGridData(
+          drawVerticalLine: false
+        ),
+          titlesData: FlTitlesData(
+              rightTitles:
+                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              topTitles:
+                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              leftTitles: const AxisTitles(
+                  axisNameWidget: Text("Monthly Sales"),
+                  axisNameSize: 30,
+                  sideTitles: SideTitles(showTitles: true, reservedSize: 33)),
+              bottomTitles: AxisTitles(
+                  axisNameWidget: const Text("Months"),
+                  axisNameSize: 30,
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 33,
+                    getTitlesWidget: (double value, meta) {
+                      return switch (value) {
+                        0 => const Text("Jan"),
+                        1 => const Text("Feb"),
+                        2 => const Text("Mar"),
+                        3 => const Text("Apr"),
+                        4 => const Text("May"),
+                        5 => const Text("Jun"),
+                        6 => const Text("Jul"),
+                        7 => const Text("Aug"),
+                        8 => const Text("Sep"),
+                        9 => const Text("Oct"),
+                        10 => const Text("Nov"),
+                        11 => const Text("Dec"),
+                        _ => const Text("")
+                      };
+                    },
+                  ))),
+          barGroups: [
+            BarChartGroupData(x: 0, barRods: [BarChartRodData(toY: 10)]),
+            BarChartGroupData(x: 1, barRods: [BarChartRodData(toY: 5)])
+          ]
+          // read about it in the BarChartData section
+          ),
+      // Optional
+    ));
   }
 }
