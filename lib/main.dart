febe15f8b9129d800c1e711784a1492fce154443
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-19 14:33:10 +0200

    Refactor main.dart to use flutter_riverpod for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stock_managment_system_app/theme.dart';
 import 'package:flutter_stock_managment_system_app/router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       routerConfig: router,
       title: 'Flutter Demo',
       theme: CustomTheme.appThemeData(context),
-    
     );
   }
 }
+
+
+
+
 
