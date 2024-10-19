7799dcc3f59b52d720c5b5725dfd6a63719fefa7
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-19 14:34:08 +0200

    Refactor InventoryItemCard to use flutter_riverpod for state management

--- lib/widgets/inventory_item_card.dart
+++ lib/widgets/inventory_item_card.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_stock_managment_system_app/utils/api_definitions.dart';
+import 'package:flutter_stock_managment_system_app/utils/data.dart';
+import 'package:go_router/go_router.dart';
 
-class InventoryItemCard extends StatelessWidget {
-  final AsyncSnapshot snapshot;
+class InventoryItemCard extends ConsumerWidget {
+  final InventoryGet value;
   final int index;
 
   const InventoryItemCard(
-      {super.key, required this.snapshot, required this.index});
+      {super.key, required this.value, required this.index});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Card(
       color: Theme.of(context).colorScheme.primaryContainer,
       child: Padding(
@@ -19,7 +24,7 @@ class InventoryItemCard extends StatelessWidget {
               radius: 40,
               backgroundColor: Theme.of(context).colorScheme.surface,
               child: Text(
-                "${snapshot.data!.inventoryItems[index]["product_name"].substring(0, 1)}",
+                "${value.inventoryItems[index]["product_name"].substring(0, 1)}",
                 style: const TextStyle(fontSize: 40),
                 overflow: TextOverflow.ellipsis,
               ),
@@ -32,13 +37,13 @@ class InventoryItemCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "${snapshot.data!.inventoryItems[index]["product_name"]}",
+                    "${value.inventoryItems[index]["product_name"]}",
                     style: Theme.of(context).textTheme.headlineSmall,
                   ),
                   Text(
-                      "Selling Price: K ${snapshot.data!.inventoryItems[index]["selling_price"]}"),
+                      "Selling Price: K ${value.inventoryItems[index]["selling_price"]}"),
                   Text(
-                      "Remaining: ${snapshot.data!.inventoryItems[index]["product_qty"]}"),
+                      "Remaining: ${value.inventoryItems[index]["product_qty"]}"),
                 ],
               ),
             ),
@@ -51,18 +56,27 @@ class InventoryItemCard extends StatelessWidget {
                     context: context,
                     builder: (BuildContext context) => AlertDialog(
                       title: Text(
-                          "Do you want to delete ${snapshot.data!.inventoryItems[index]["product_name"]}"),
+                          "Do you want to delete ${value.inventoryItems[index]["product_name"]}"),
                       content: Text(
-                          "This action will permanently delete ${snapshot.data!.inventoryItems[index]["product_name"]} "),
+                          "This action will permanently delete ${value.inventoryItems[index]["product_name"]} "),
                       actions: [
                         FilledButton(
-                            onPressed: () => print("hello"),
+                            onPressed: () {
+                              ref
+                                  .read(inventoryItemsProvider.notifier)
+                                  .deleteInventoryItem(value
+                                      .inventoryItems[index]["product_id"]);
+                              context.go("/inventory");
+                              Navigator.of(context).pop();
+                            },
                             style: FilledButton.styleFrom(
                                 backgroundColor: Colors.red),
                             child: const Text("Delete")),
                         OutlinedButton(
-                            onPressed: () => print("hello there"),
-                          
+                            onPressed: () => {
+                                  context.go("/inventory"),
+                                  Navigator.of(context).pop()
+                                },
                             child: const Text("Cancel"))
                       ],
                     ),
