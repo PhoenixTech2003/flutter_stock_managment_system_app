03144f88b263c790cd0b132c3ae8a12005f5d52a
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-19 14:33:33 +0200

    Refactor InventoryPage to use flutter_riverpod for state management

--- lib/pages/inventory.dart
+++ lib/pages/inventory.dart
@@ -2,57 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_stock_managment_system_app/utils/api_definitions.dart';
 import 'package:flutter_stock_managment_system_app/utils/data.dart';
 import 'package:flutter_stock_managment_system_app/widgets/inventory_item_card.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class InventoryPage extends StatefulWidget {
+class InventoryPage extends ConsumerWidget {
   const InventoryPage({super.key});
-
   @override
-  State<InventoryPage> createState() => _InventoryPageState();
-}
-
-class _InventoryPageState extends State<InventoryPage> {
-  late Future<InventoryGet> futureInventoryGet;
-
-  @override
-  void initState() {
-    super.initState();
-    futureInventoryGet = fetchInventory();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: futureInventoryGet,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Inventory",
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      )),
-                ),
-                Expanded(
-                  flex: 9,
-                  child: ListView.builder(
-                    padding: const EdgeInsets.all(9),
-                    itemCount: snapshot.data!.inventoryItems.length,
-                    itemBuilder: (BuildContext context, int index) {
-                     return InventoryItemCard(snapshot: snapshot, index: index);
-                    },
-                  ),
-                ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-
-          return const CircularProgressIndicator();
-        });
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<InventoryGet> inventory =
+        ref.watch(inventoryItemsProvider);
+    return Container(
+        child: switch (inventory) {
+      AsyncData(:final value) => Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Inventory",
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  )),
+            ),
+            Expanded(
+              flex: 9,
+              child: ListView.builder(
+                padding: const EdgeInsets.all(9),
+                itemCount: value.inventoryItems.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return InventoryItemCard(
+                    value: value,
+                    index: index,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      AsyncError() => const Text("Failed to fetch Inventory data"),
+      _ => const CircularProgressIndicator()
+    });
   }
 }
