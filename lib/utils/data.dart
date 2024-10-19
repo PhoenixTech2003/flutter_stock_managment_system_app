ebe79063a1a606b5beb95a28bc4a56058ba91207
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-19 14:33:45 +0200

    Refactor data.dart to use flutter_riverpod for state management

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_stock_managment_system_app/utils/api_definitions.dart';
 
-Future<Summary> fetchSummary() async {
+part 'data.g.dart';
+
+@riverpod
+Future<Summary> fetchSummary(FetchSummaryRef ref) async {
   final response =
       await http.get(Uri.parse('http://localhost:9000/v1/summary/'));
 
@@ -18,13 +22,32 @@ Future<Summary> fetchSummary() async {
   }
 }
 
-Future<InventoryGet> fetchInventory() async {
-  final response =
-      await http.get(Uri.parse("http://localhost:9000/v1/inventory/"));
+@riverpod
+class InventoryItems extends _$InventoryItems {
+  @override
+  Future<InventoryGet> build() async {
+    final response =
+        await http.get(Uri.parse("http://localhost:9000/v1/inventory/"));
 
-  if (response.statusCode == 200) {
-    return InventoryGet.fromJson(jsonDecode(response.body) as List<dynamic>);
-  } else {
-    throw Exception("Failed to load inventory items");
+    if (response.statusCode == 200) {
+      return InventoryGet.fromJson(jsonDecode(response.body) as List<dynamic>);
+    } else {
+      throw Exception("Failed to load inventory items");
+    }
+  }
+
+  Future<void> deleteInventoryItem(String productId) async {
+    final response = await http.delete(
+        Uri.parse('http://localhost:9000/v1/inventory/$productId'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+    if (response.statusCode == 200) {
+      ref.invalidateSelf();
+      await future;
+    } else {
+      throw Exception("Failed to delete item");
+    }
   }
 }
